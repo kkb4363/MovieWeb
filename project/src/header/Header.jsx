aee591994b4a5839bee7d08d3344f0cd0698fefa
gibeom <101088024+kkb4363@users.noreply.github.com>
2023-10-09 16:19:13 +0900

    init

---
+++ project/src/header/Header.jsx
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 5vh;
+
+  background-color: rgb(45 45 45);
+`;
+
+const Header = () => {
+  return <Wrapper className="">hi</Wrapper>;
+};
+
+export default Header;
